b53f8766b113d6d84ef0bb293a4e07358aaa2ec7
mkelepe <mkelepeshis2@gmail.com>
2022-06-15 06:07:11 +0300

    Update

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-'use strict';
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const express = require('express');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
-app.get('/', async (req, res) => {
-  res.status(200).send('CSU-Hackfest-Lab-App is running! ').end();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// Start the server
-const PORT = process.env.PORT || 8090;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
-
 
 module.exports = app;
