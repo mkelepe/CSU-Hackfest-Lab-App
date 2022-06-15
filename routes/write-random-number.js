2513476f120cd790a638b0e7e9512b8d0e0e3b3e
mkelepe <mkelepeshis2@gmail.com>
2022-06-15 18:32:58 +0300

    Update

--- routes/write-random-number.js
+++ routes/write-random-number.js
@@ -2,35 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* POST random number to DB. */
-router.post('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
 
-  var random_number= Math.floor(Math.random() * 1000);
+  try {
+    var random_number= Math.floor(Math.random() * 1000);
   
-  var sql = require("mssql");
-
-  // config for your database
-  var config = {
-      user: process.env.db_user || 'dbadminuser',
-      password: process.env.db_password || '44!,h{/FugEVnw+V',
-      server: process.env.db_host || 'sqlserver7ukmp7nzntfpk.database.windows.net', 
-      database: process.env.db_name || 'sampledb' 
-  };
-
-  // connect to your database
-  sql.connect(config, function (err) {
+    var sql = require("mssql");
   
-      if (err) console.log(err);
-
-      // create Request object
-      var request = new sql.Request();
-         
-      // query to the database and get the records
-      request.query(`INSERT INTO mytable ([key], value) VALUES (${random_number}, ${random_number});`, function (err, recordset) {
-          if (err) console.log(err)
-          // send records as a response
-          res.send({"Message" : `Successfully written number ${random_number}`});
-      });
-  });
+    // config for your database
+    var config = {
+        user: process.env.db_user || 'dbadminuser',
+        password: process.env.db_password || '44!,h{/FugEVnw+V',
+        server: process.env.db_host || 'sqlserver7ukmp7nzntfpk.database.windows.net', 
+        database: process.env.db_name || 'sampledb' 
+    };
+  
+    // connect to your database
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+  
+        // create Request object
+        var request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query(`INSERT INTO mytable ([key], value) VALUES (${random_number}, ${random_number});`, function (err, recordset) {
+            if (err) console.log(err)
+            // send records as a response
+            res.send({"Message" : `Successfully written number ${random_number}`});
+        });
+    });
+  } catch (error) {
+      console.log('My error:')
+      console.log(error)
+      res.status(200).send({"Error" : JSON.stringify(error)});
+  }
 
 });
 
