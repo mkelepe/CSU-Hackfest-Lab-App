b67afe14296d02e7d547c28eb922f71339cd2758
mkelepe <mkelepeshis2@gmail.com>
2022-06-15 06:36:09 +0300

    Update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.status(200).send('CSU-Hackfest-Lab-App is running!').end();
+  res.status(200).send('CSU-Hackfest-Lab-App is running!' + process.env.DefaultConnection).end();
 });
 
 module.exports = router;
